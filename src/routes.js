1d1c4428f9bb42b1dfdd3540193341351c0c9223
Victor <vicbem@hotmail.com>
2022-02-06 17:56:22 -0300

    Correção do nome Autorization para Authorization

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,16 @@ const routes = express.Router();
 const TaskController = require("./controllers/TaskController");
 const UserController = require("./controllers/UserController");
 
-const autorizationMiddleware = require("./controllers/AutorizationMiddlewares");
+const authorizationMiddlewares = require("./controllers/AuthorizationMiddlewares");
 
-routes.get('/', autorizationMiddleware.bearer, TaskController.index);
+routes.get('/', authorizationMiddlewares.bearer, TaskController.index);
 routes.post('/task/create/:status', TaskController.create);
 routes.post('/task/update/:id', TaskController.update);
 routes.post('/task/delete/:id', TaskController.delete);
 
 routes.get("/login", UserController.screenLogin);
-routes.post('/user/login', autorizationMiddleware.local, UserController.login);
-routes.get('/user/logout', autorizationMiddleware.bearer, UserController.logout);
+routes.post('/user/login', authorizationMiddlewares.local, UserController.login);
+routes.get('/user/logout', authorizationMiddlewares.bearer, UserController.logout);
 
 routes.get('/user', UserController.get);
 routes.post('/user', UserController.create);
